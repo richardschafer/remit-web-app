9f45139e3d71834acb7b2f3b9d9c5557c7e23ba8
Richard Schafer <ri.sch@icloud.com>
2018-12-20 18:18:48 +0100

    setup babel and pm2 npm script and basic express server

---
+++ src/views/index.js
@@ -0,0 +1,23 @@
+export default function renderHtml() {
+  return `
+    <!doctype html>
+
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+    
+      <title>The HTML5 Herald</title>
+      <meta name="description" content="The HTML5 Herald">
+      <meta name="author" content="SitePoint">
+    
+      <link rel="stylesheet" href="css/styles.css?v=1.0">
+    
+    </head>
+    
+    <body>
+      <h2>hello express</h2>
+      <script src="js/scripts.js"></script>
+    </body>
+    </html>
+  `;
+}
