b2ae673faaa888a69cee9bee033b814d2f32e010
Richard Schafer <ri.sch@icloud.com>
2018-12-28 22:36:55 +0100

    get variables from process.env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   GRAPHQL_ENDPOINT_URI: process.env.GRAPHQL_ENDPOINT_URI || '',
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
+  FIREBASE_DB_URL: process.env.FIREBASE_DB_URL || '',
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
 };
